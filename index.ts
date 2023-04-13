90d7a8f355ee69481e37487467792657fabec26e
John Van Der Loo <jvanderloo@gmail.com>
2023-04-13 15:24:57 +1000

    Initial commit

---
+++ index.ts
@@ -0,0 +1,36 @@
+function repeatedString(s: string, n: number): number {
+  const STRING_TO_FIND = "a";
+  const STRING_TO_FIND_RE = new RegExp(STRING_TO_FIND, "gi");
+  if (s.length === 1 && s === STRING_TO_FIND) {
+    return n;
+  }
+
+  // rather than constructing a string of a given length,
+  // we can figure out how many times that string wholely fits
+  // in that length and then figure out the remainder to get the
+  // remaining substring to search
+  const baseMultiplier = Math.floor(n / s.length);
+  const remainingString = s.substring(0, n % s.length);
+  const baseStringMatch = s.match(STRING_TO_FIND_RE);
+  const baseCount = baseStringMatch
+    ? baseStringMatch.length * baseMultiplier
+    : 0;
+  const remainingStringMatch = remainingString.match(STRING_TO_FIND_RE);
+  const remainingCount = remainingStringMatch ? remainingStringMatch.length : 0;
+
+  return baseCount + remainingCount;
+}
+
+function logWithAssert(str: string, num: number, expectedOutput: number) {
+  const output = repeatedString(str, num);
+  console.log(output, output === expectedOutput);
+}
+
+logWithAssert("aba", 10, 7);
+logWithAssert("a", 10, 10);
+logWithAssert("a", 10000000000000, 10000000000000);
+logWithAssert(
+  "kmretasscityylpdhuwjirnqimlkcgxubxmsxpypgzxtenweirknjtasxtvxemtwxuarabssvqdnktqadhyktagjxoanknhgilnm",
+  736778906400,
+  51574523448
+);
